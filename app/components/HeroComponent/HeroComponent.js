b3caf6d7fd5ae89786111720af225421e7856a03
feiandart <fabriziafisichella@gmail.com>
2024-06-26 20:04:44 +0200

    feat: add carousel

--- app/components/HeroComponent/HeroComponent.js
+++ app/components/HeroComponent/HeroComponent.js
@@ -11,7 +11,7 @@ const HeroComponent = () => {
   return (
     <div className={styles.heroContainer}>
       <h2>Titolo film</h2>
-      <img src={`https://picsum.photos/1000/300`} alt="hero-img" />
+      <img src={`https://picsum.photos/1000/1000`} alt="hero-img" />
       <button /* onClick={handleButtonClick} */ className={styles.heroBtn}>
         Discover more
       </button>
--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
+import Carousel from "./components/Carousel/Carousel";
 import HeroComponent from "./components/HeroComponent/HeroComponent";
 import styles from "./page.module.scss";
 
 export default function Home() {
   return (
-    <main>
+    <main className={styles.mainContainer}>
       <HeroComponent />
+      <section className={styles.carouselSection}>
+        <Carousel />
+      </section>
     </main>
   );
 }
